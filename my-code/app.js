fe16ed07dfdbc9e9eccb3d3bba0eb046a8793daa
Gourgey <joshuagourgey@gmail.com>
2018-10-15 20:58:01 +0100

    got wall block and basic hole to work

--- my-code/app.js
+++ my-code/app.js
@@ -2,10 +2,6 @@
 const myContainer = document.querySelector('#container');
 
 
-
-
-
-
 function createGrid(x) {
   for (let rows = 0; rows < x; rows++) {
     for (let columns = 0; columns < x; columns++) {
@@ -37,15 +33,26 @@ const myCrateSpot = {
 };
 
 
-// for tracking the characters and all, make a variable that you use in your dom
+const myHoleSpot = {
+  row: 8,
+  column: 8
+};
+
+
+// for tracking the characters and all, make a variable that you use in your dom.
+// get the div for the character with the following variables then change the colour to red and give it a class of active for the image.
 let currentCharacterPosition = document.querySelector(`div[rowid="${myPlayerSpot.row}"][columnid="${myPlayerSpot.column}"]`);
 // currentCharacterPosition.classList.add('active');
 console.log(currentCharacterPosition);
 currentCharacterPosition.style.backgroundColor = 'red';
 
+// get the div for the crate, colour it green and make it a let so you can reassign the let to another div.
 let currentCratePosition = document.querySelector(`div[rowid="${myCrateSpot.row}"][columnid="${myCrateSpot.column}"]`);
 currentCratePosition.style.backgroundColor = 'green';
 
+// get the div for the hole and colour it black.
+let currentHolePosition = document.querySelector(`div[rowid="${myHoleSpot.row}"][columnid="${myHoleSpot.column}"]`);
+currentHolePosition.style.backgroundColor = 'black';
 
 
 
@@ -58,93 +65,168 @@ function moveColor() {
 
 
 
-
-
-
 window.addEventListener('keydown', function(e) {
-  console.log(e.which);
+  // console.log(e.which);
   if (e.which === 38) {
     event.preventdefault;
-    myPlayerSpot.row --;
-    //stops character going over the edge - cant be === 0 becuase the ++ will make it row 1.
-    if (myPlayerSpot.row === -1) {
-      myPlayerSpot.row ++;
+
+    // stops character moving onto crate when crate is at wall and allows code to run otherwise.
+    if (myCrateSpot.row === 0
+      && myPlayerSpot.row === 1
+      && myCrateSpot.column === myPlayerSpot.column) {
+      console.log('dont move!');
+    } else {
+      // MOVES UP
+      myPlayerSpot.row --;
+
+
+      //stops character going over the edge - cant be === 0 becuase the ++ will make it row 1.
+      if (myPlayerSpot.row === -1) {
+        myPlayerSpot.row ++;
+      }
+
+
+      // gets rid of current square colour then by reassigning takes new vairable values (from keydown listener) to make a new div colour with red.
+      moveColor();
+
+
+      if (myPlayerSpot.row === myCrateSpot.row
+         && myPlayerSpot.column === myCrateSpot.column) {
+
+        // moves in character direction - when character moves onto crate (code above) - moves crate up and makes new square the current square then makes it green.
+        myCrateSpot.row --;
+
+        // don't need to turn this square white as the charater will take over then to white when it leaves.
+        currentCratePosition = document.querySelector(`div[rowid="${myCrateSpot.row}"][columnid="${myCrateSpot.column}"]`);
+        currentCratePosition.style.backgroundColor = 'green';
+      }
     }
-    // MOVES UP
-    // gets rid of current square colour then by reassigning takes new vairable values (from keydown listener) to make a new div colour with red.
-    moveColor();
 
-
-    if (currentCharacterPosition === currentCratePosition) {
-
-      // when character and crate positions are equal - moves it right and makes new square the current square then makes it green.
-      myCrateSpot.row --;
-      // don't need to turn this square white as the charater will take over then to white when it leaves.
+    // if the crate position === the hole position - make the position black to cover the green, change the crate vars to 0, 0 by
+    // ----- minusing the hol vars then reassign the crate position and colour so it shows up at the top right
+    if (myCrateSpot.row === myHoleSpot.row
+       && myCrateSpot.column === myHoleSpot.column) {
+      currentHolePosition.style.backgroundColor = 'black';
+      myCrateSpot.row -= myHoleSpot.row;
+      myCrateSpot.column -= myHoleSpot.column;
       currentCratePosition = document.querySelector(`div[rowid="${myCrateSpot.row}"][columnid="${myCrateSpot.column}"]`);
       currentCratePosition.style.backgroundColor = 'green';
     }
-
-    // stops crate going over edge
-    if (myCrateSpot.row === -1) {
-      myCrateSpot.row ++;
-      console.log(myCrateSpot);
-    }
-
-    // if (myCrateSpot.row === 0 && myPlayerSpot.row === 1) {
-    //   console.log(myPlayerSpot);
-    // }
 
 
   } else if (e.which === 40) {
     event.preventdefault;
-    if (myPlayerSpot.row === 9) {
-      myPlayerSpot.row --;
-    }
-    // MOVES DOWN
-    myPlayerSpot.row ++;
-    moveColor();
-    if (currentCharacterPosition === currentCratePosition) {
-      if (myCrateSpot.row === 0) {
-        myCrateSpot.row --;
+
+    if (myCrateSpot.row === 9
+      && myPlayerSpot.row === 8
+      && myCrateSpot.column === myPlayerSpot.column) {
+      console.log('dont move!');
+    } else {
+
+      // MOVES DOWN
+      myPlayerSpot.row ++;
+
+      if (myPlayerSpot.row === 10) {
+        myPlayerSpot.row --;
       }
-      myCrateSpot.row ++;
+
+      moveColor();
+
+      if (myPlayerSpot.row === myCrateSpot.row
+        && myPlayerSpot.column === myCrateSpot.column) {
+
+        myCrateSpot.row ++;
+
+        currentCratePosition = document.querySelector(`div[rowid="${myCrateSpot.row}"][columnid="${myCrateSpot.column}"]`);
+        currentCratePosition.style.backgroundColor = 'green';
+      }
+    }
+
+    if (myCrateSpot.row === myHoleSpot.row
+       && myCrateSpot.column === myHoleSpot.column) {
+      currentHolePosition.style.backgroundColor = 'black';
+      myCrateSpot.row -= myHoleSpot.row;
+      myCrateSpot.column -= myHoleSpot.column;
       currentCratePosition = document.querySelector(`div[rowid="${myCrateSpot.row}"][columnid="${myCrateSpot.column}"]`);
       currentCratePosition.style.backgroundColor = 'green';
     }
+
 
   } else if (e.which === 37) {
     event.preventdefault;
-    if (myPlayerSpot.column === 0) {
-      myPlayerSpot.column ++;
-    }
-    // MOVES LEFT
-    myPlayerSpot.column --;
-    moveColor();
-    if (currentCharacterPosition === currentCratePosition) {
-      if (myCrateSpot.row === 0) {
-        myCrateSpot.column --;
+
+    if (myCrateSpot.column === 0
+      && myPlayerSpot.column === 1
+      && myCrateSpot.row === myPlayerSpot.row) {
+      console.log('dont move!');
+    } else {
+
+      // MOVES LEFT
+      myPlayerSpot.column --;
+
+      if (myPlayerSpot.column === -1) {
+        myPlayerSpot.column ++;
       }
-      myCrateSpot.column --;
+
+      moveColor();
+
+      if (myPlayerSpot.row === myCrateSpot.row
+        && myPlayerSpot.column === myCrateSpot.column) {
+
+        myCrateSpot.column --;
+
+        currentCratePosition = document.querySelector(`div[rowid="${myCrateSpot.row}"][columnid="${myCrateSpot.column}"]`);
+        currentCratePosition.style.backgroundColor = 'green';
+      }
+    }
+
+    if (myCrateSpot.row === myHoleSpot.row
+       && myCrateSpot.column === myHoleSpot.column) {
+      currentHolePosition.style.backgroundColor = 'black';
+      myCrateSpot.row -= myHoleSpot.row;
+      myCrateSpot.column -= myHoleSpot.column;
       currentCratePosition = document.querySelector(`div[rowid="${myCrateSpot.row}"][columnid="${myCrateSpot.column}"]`);
       currentCratePosition.style.backgroundColor = 'green';
     }
 
+
   } else if (e.which === 39) {
     event.preventdefault;
-    if (myPlayerSpot.column === 9) {
-      myPlayerSpot.column --;
-    }
-    // MOVES RIGHT
-    myPlayerSpot.column ++;
-    moveColor();
-    if (currentCharacterPosition === currentCratePosition) {
-      if (myCrateSpot.row === 0) {
-        myCrateSpot.column ++;
+
+    if (myCrateSpot.column === 9
+      && myPlayerSpot.column === 8
+      && myCrateSpot.row === myPlayerSpot.row) {
+      console.log('dont move!');
+    } else {
+
+      // MOVES RIGHT
+      myPlayerSpot.column ++;
+
+      if (myPlayerSpot.column === 10) {
+        myPlayerSpot.column --;
       }
-      myCrateSpot.column ++;
+
+      moveColor();
+
+      if (myPlayerSpot.row === myCrateSpot.row
+        && myPlayerSpot.column === myCrateSpot.column) {
+
+        myCrateSpot.column ++;
+
+        currentCratePosition = document.querySelector(`div[rowid="${myCrateSpot.row}"][columnid="${myCrateSpot.column}"]`);
+        currentCratePosition.style.backgroundColor = 'green';
+      }
+    }
+
+    if (myCrateSpot.row === myHoleSpot.row
+       && myCrateSpot.column === myHoleSpot.column) {
+      currentHolePosition.style.backgroundColor = 'black';
+      myCrateSpot.row -= myHoleSpot.row;
+      myCrateSpot.column -= myHoleSpot.column;
       currentCratePosition = document.querySelector(`div[rowid="${myCrateSpot.row}"][columnid="${myCrateSpot.column}"]`);
       currentCratePosition.style.backgroundColor = 'green';
     }
+
   }
 });
 
